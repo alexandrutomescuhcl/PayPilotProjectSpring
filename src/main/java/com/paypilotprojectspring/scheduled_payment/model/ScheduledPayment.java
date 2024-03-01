fd576d9eda9cb2106c9ad2d0bf5a5aa6250c9b8b
ivona.racasean <ivona_racasean9@yahoo.com>
2024-03-01 15:12:05 +0200

    Used lombok!

--- src/main/java/com/paypilotprojectspring/scheduled_payment/model/ScheduledPayment.java
+++ src/main/java/com/paypilotprojectspring/scheduled_payment/model/ScheduledPayment.java
@@ -6,9 +6,13 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(schema = "SPRING", name = "SCHEDULED_PAYMENT")
+@Getter
+@Setter
 public class ScheduledPayment {
     @Id
     private int id;
@@ -68,109 +72,4 @@ public class ScheduledPayment {
         this.paymentMethod = paymentMethod;
         this.payerAccount = payerAccount;
     }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getUserId() {
-        return userId;
-    }
-
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
-
-    public int getBillId() {
-        return billId;
-    }
-
-    public void setBillId(int billId) {
-        this.billId = billId;
-    }
-
-    public String getNameOfTheBill() {
-        return nameOfTheBill;
-    }
-
-    public void setNameOfTheBill(String nameOfTheBill) {
-        this.nameOfTheBill = nameOfTheBill;
-    }
-
-    public String getPurposeOfPayment() {
-        return purposeOfPayment;
-    }
-
-    public void setPurposeOfPayment(String purposeOfPayment) {
-        this.purposeOfPayment = purposeOfPayment;
-    }
-
-    public String getPayeeName() {
-        return payeeName;
-    }
-
-    public void setPayeeName(String payeeName) {
-        this.payeeName = payeeName;
-    }
-
-    public String getPayeeAddress() {
-        return payeeAddress;
-    }
-
-    public void setPayeeAddress(String payeeAddress) {
-        this.payeeAddress = payeeAddress;
-    }
-
-    public String getBillDate() {
-        return billDate;
-    }
-
-    public void setBillDate(String billDate) {
-        this.billDate = billDate;
-    }
-
-    public PaymentFrequency getPaymentFrequency() {
-        return paymentFrequency;
-    }
-
-    public void setPaymentFrequency(PaymentFrequency paymentFrequency) {
-        this.paymentFrequency = paymentFrequency;
-    }
-
-    public String getPaymentMethod() {
-        return paymentMethod;
-    }
-
-    public void setPaymentMethod(String paymentMethod) {
-        this.paymentMethod = paymentMethod;
-    }
-
-    public String getPayerAccount() {
-        return payerAccount;
-    }
-
-    public void setPayerAccount(String payerAccount) {
-        this.payerAccount = payerAccount;
-    }
-
-    public int getBankDetailsId() {
-        return bankDetailsId;
-    }
-
-    public void setBankDetailsId(int bankDetailsId) {
-        this.bankDetailsId = bankDetailsId;
-    }
-
-    public Boolean getEnabled() {
-        return enabled;
-    }
-
-    public void setEnabled(Boolean enabled) {
-        this.enabled = enabled;
-    }
-
 }
