0c27af741bf38fc11ecde37e7d44ece26721efd9
octavians22 <89181763+octavians22@users.noreply.github.com>
2024-03-04 12:46:01 +0200

    BillGroceries and BillRetirement APIs #feature

--- src/main/java/com/paypilotprojectspring/bill/controller/BillRetirementController.java
+++ src/main/java/com/paypilotprojectspring/bill/controller/BillRetirementController.java
@@ -5,10 +5,9 @@ import com.paypilotprojectspring.bill.dto.BillRetirementDTO;
 import com.paypilotprojectspring.bill.service.BillGroceriesService;
 import com.paypilotprojectspring.bill.service.BillRetirementService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -27,5 +26,20 @@ public class BillRetirementController {
     @GetMapping("/{id}")
     public Optional<BillRetirementDTO> getRetirementBillById(@PathVariable Long id){
         return billRetirementService.findById(id);
+    }
+
+    @PostMapping("/addBill")
+    public ResponseEntity<Void> addBill(@RequestBody BillRetirementDTO billRetirementDTO){
+        billRetirementService.addBill(billRetirementDTO);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+    @PutMapping("/updateBill/{id}")
+    public ResponseEntity<Boolean> updateBill(@PathVariable Long id, @RequestBody BillRetirementDTO billRetirementDTO){
+        return new ResponseEntity<>(billRetirementService.updateBill(billRetirementDTO, id),HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/deleteBill/{id}")
+    public ResponseEntity<Boolean> deleteBill(@PathVariable Long id){
+        return new ResponseEntity<>(billRetirementService.deleteBill(id), HttpStatus.OK);
     }
 }
